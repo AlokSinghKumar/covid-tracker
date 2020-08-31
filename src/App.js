94657915808e09afefbe9b3103347e9eea4916f5
Alok_singh <ALOK743165@GMAIL.COM>
2020-08-31 17:31:33 +0530

    API calls and graphs

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import { FormControl, MenuItem, Select, Card, CardContent} from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import InfoBox from './Infobox';
+import Map from './Map';
+import Table from "./Table";
+import {sortData} from "./helper/util"
+import LineGraph from "./LineGraph"
+
+function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+
+// ------------------------------------------------
+// -----------FOR WORLDWIDE BY DEFAULT-------------
+// ------------------------------------------------
+    useEffect(() => {
+        fetch ("https://disease.sh/v3/covid-19/all")
+        .then(response => response.json())
+        .then((data) => {
+            setCountryInfo(data);
+        })
+    }, [])
+
+
+// ------------------------------------------------
+// -----------FOR DROP DOWN TO SELECT--------------
+// ------------------------------------------------
+  useEffect(() => {
+    //The code inside here will run once
+    // when the component loads and not again after
+
+    // async -> send request, wait for it, so something with the data
+
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, [])
+
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value
+    setCountry(countryCode);
+
+    const url = countryCode === 'worldwide' 
+    ? "https://disease.sh/v3/covid-19/all"
+    : `https://disease.sh/v3/covid-19/countries/${countryCode}?strict=true`
+  
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        setCountryInfo(data);
+    })
+}
+
+  return (
+    <div className="app">
+        <div className="app__left">
+            <div className="app__header">
+            <h1>COVID-19 TRACKER</h1>
+            <FormControl>
+                <Select 
+                    varient="outline"
+                    value={country}
+                    onChange={onCountryChange}
+                >
+                    <MenuItem value="worldwide">worldwide</MenuItem>
+
+                    {/* loop through all the countries and drop down list of the option */}
+                    {
+                    countries.map(country => (
+                        <MenuItem value={country.value}>{country.name}</MenuItem>
+                    ))
+                    }
+                </Select>
+            </FormControl> 
+            </div>
+            
+            {/* Info box for each country*/}
+            <div className="app__stats">
+                <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+                <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+                <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+            </div>
+
+            {/* Map of each country */}
+            <Map />
+        </div>
+
+        <Card className="app__right">
+            <CardContent>
+                <h3>Live Cases by Country</h3>
+                <Table countries={tableData}></Table>
+                <h3>worldwide new cases</h3>    
+                {/* Graph */}
+                <LineGraph />
+            </CardContent>
+        </Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Infobox.js
@@ -0,0 +1,28 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+import './Infobox.css';
+
+function InfoBox({title, cases, total}){
+    return(
+        <div>
+            <Card classname="infoBox">
+                <CardContent>
+                    {/* Title of the card */}
+                    <Typography className="infoBox__title" color="textSecondary">
+                        {title}
+                    </Typography>
+
+                    {/* Number if cases */}
+                    <h2 className="infoBox__cases">{cases}</h2>
+
+                    {/* Total number of cases */}
+                    <Typography className="infoBox__total" color="textSecondary">
+                        {total} Total
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default InfoBox;
